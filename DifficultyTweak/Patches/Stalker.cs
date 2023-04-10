165e07104c0f15f723b7462d7e52c285767be840
eternalUnion <dnzbyk12345@gmail.com>
2023-04-11 01:34:43 +0300

    Stalker

---
+++ DifficultyTweak/Patches/Stalker.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DifficultyTweak.Patches
+{
+    [HarmonyPatch(typeof(Stalker), nameof(Stalker.SandExplode))]
+    public class Stalker_SandExplode_Patch
+    {
+        static bool Prefix(Stalker __instance, ref int ___difficulty, ref EnemyIdentifier ___eid)
+        {
+            ___difficulty = 4;
+
+            return true;
+        }
+    }
+}
