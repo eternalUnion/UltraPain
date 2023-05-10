65fc807b613e0f64887bab9815492061adee329a
eternalUnion <dnzbyk12345@gmail.com>
2023-05-10 21:25:45 +0300

    Attempted to add a new predicted projectile system similar to cerberus

--- Ultrapain/Patches/StreetCleaner.cs
+++ Ultrapain/Patches/StreetCleaner.cs
@@ -66,7 +66,7 @@ namespace Ultrapain.Patches
 
     class BulletCheck_OnTriggerEnter_Patch
     {
-        static void Postfix(BulletCheck __instance, Collider __0)
+        static void Postfix(BulletCheck __instance, Collider __0/*, EnemyIdentifier ___eid*/)
         {
             if (!(__instance.type == CheckerType.Streetcleaner && __0.gameObject.layer == 14))
                 return;
@@ -77,7 +77,8 @@ namespace Ultrapain.Patches
                 grn.enemy = true;
                 grn.CanCollideWithPlayer(true);
 
-                Rigidbody rb = __0.GetComponent<Rigidbody>();
+                // OLD PREDICTION
+                /*Rigidbody rb = __0.GetComponent<Rigidbody>();
                 float magnitude = rb.velocity.magnitude;
 
                 //float distance = Vector3.Distance(MonoSingleton<PlayerTracker>.Instance.gameObject.transform.position, __0.transform.position);
@@ -87,7 +88,14 @@ namespace Ultrapain.Patches
                 float velocity = Mathf.Clamp(distance, Mathf.Max(magnitude - 5.0f, 0), magnitude + 5);
 
                 __0.transform.LookAt(predictedPosition);
-                rb.velocity = __0.transform.forward * velocity;
+                rb.velocity = __0.transform.forward * velocity;*/
+
+                // NEW PREDICTION
+                Vector3 predictedPosition = Tools.PredictPlayerPosition(1);
+                __0.transform.LookAt(predictedPosition);
+                Rigidbody rb = __0.GetComponent<Rigidbody>();
+                rb.velocity = Vector3.zero;
+                rb.AddForce(__0.transform.forward * 20000f /* * ___eid.totalSpeedModifier */);
             }
         }
     }
--- Ultrapain/Plugin.cs
+++ Ultrapain/Plugin.cs
@@ -653,6 +653,44 @@ namespace Ultrapain
         }
     }
 
+    public static class Tools
+    {
+        private static Transform _target;
+        private static Transform target { get
+            {
+                if(_target == null)
+                    _target = MonoSingleton<PlayerTracker>.Instance.GetTarget();
+                return _target;
+            }
+        }
+
+        public static Vector3 PredictPlayerPosition(float speedMod, Collider enemyCol = null)
+        {
+            Vector3 projectedPlayerPos;
+
+            if (MonoSingleton<PlayerTracker>.Instance.GetPlayerVelocity().magnitude == 0f)
+            {
+                return target.position;
+            }
+            RaycastHit raycastHit;
+            if (enemyCol != null && Physics.Raycast(target.position, MonoSingleton<PlayerTracker>.Instance.GetPlayerVelocity(), out raycastHit, MonoSingleton<PlayerTracker>.Instance.GetPlayerVelocity().magnitude * 0.35f / speedMod, 4096, QueryTriggerInteraction.Collide) && raycastHit.collider == enemyCol)
+            {
+                projectedPlayerPos = target.position;
+            }
+            else if (Physics.Raycast(target.position, MonoSingleton<PlayerTracker>.Instance.GetPlayerVelocity(), out raycastHit, MonoSingleton<PlayerTracker>.Instance.GetPlayerVelocity().magnitude * 0.35f / speedMod, LayerMaskDefaults.Get(LMD.EnvironmentAndBigEnemies), QueryTriggerInteraction.Collide))
+            {
+                projectedPlayerPos = raycastHit.point;
+            }
+            else
+            {
+                projectedPlayerPos = target.position + MonoSingleton<PlayerTracker>.Instance.GetPlayerVelocity() * 0.35f / speedMod;
+                projectedPlayerPos = new Vector3(projectedPlayerPos.x, target.transform.position.y + (target.transform.position.y - projectedPlayerPos.y) * 0.5f, projectedPlayerPos.z);
+            }
+
+            return projectedPlayerPos;
+        }
+    }
+
     // Asset destroyer tracker
     /*[HarmonyPatch(typeof(UnityEngine.Object), nameof(UnityEngine.Object.Destroy), new Type[] { typeof(UnityEngine.Object) })]
     public class TempClass1
