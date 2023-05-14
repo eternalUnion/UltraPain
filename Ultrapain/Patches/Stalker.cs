16fbfad550b1715499adce48cc48fa1c2983e4e7
eternalUnion <dnzbyk12345@gmail.com>
2023-05-14 03:30:03 +0300

    Stalker behind the door on GOD DAMN THE SUN has no longer blast immunity

--- Ultrapain/Patches/Stalker.cs
+++ Ultrapain/Patches/Stalker.cs
@@ -6,7 +6,12 @@ namespace Ultrapain.Patches
     {
         static bool Prefix(Stalker __instance, ref int ___difficulty, ref EnemyIdentifier ___eid)
         {
-            ___difficulty = 4;
+            if (!(StockMapInfo.Instance != null && StockMapInfo.Instance.levelName == "GOD DAMN THE SUN"
+                && __instance.transform.parent != null && __instance.transform.parent.name == "Wave 1"
+                && __instance.transform.parent.parent != null && __instance.transform.parent.parent.name.StartsWith("5 Stuff")))
+                ___difficulty = 4;
+            else
+                ___difficulty = 3;
             return true;
         }
     }
