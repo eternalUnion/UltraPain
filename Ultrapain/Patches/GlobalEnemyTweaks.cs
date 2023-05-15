3b1fc526b7a1bab3c28c1fae8348f9d334de609b
eternalUnion <dnzbyk12345@gmail.com>
2023-05-15 15:39:00 +0300

    V2 no longer gets damaged from its own shots

--- Ultrapain/Patches/GlobalEnemyTweaks.cs
+++ Ultrapain/Patches/GlobalEnemyTweaks.cs
@@ -13,6 +13,16 @@ namespace Ultrapain.Patches
         static void Postfix(EnemyIdentifier __instance)
         {
             EidStatContainer container = ConfigManager.enemyStats[__instance.enemyType];
+            
+            if(__instance.enemyType == EnemyType.V2)
+            {
+                V2 comp = __instance.GetComponent<V2>();
+                if(comp != null && comp.secondEncounter)
+                {
+                    container = ConfigManager.enemyStats[EnemyType.V2Second];
+                }
+            }
+
             __instance.totalHealthModifier *= container.health.value;
             __instance.totalDamageModifier *= container.damage.value;
             __instance.totalSpeedModifier *= container.speed.value;
